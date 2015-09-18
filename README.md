aad1300001268da682769d24959bac56fd6293fb
nguyen.vinh.nam@spectos.com <nguyen.vinh.nam@spectos.com>
2015-09-19 00:08:43 +0700

    add perplexity calculation

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
-# Usage 
+# language Modeling
 
-sudo node triGram.js 
+This is a small project I started while learning Text Mining. 
+It has no real purpose other than educational.
+The idea of is to train two corpora (Brown and Reuter) into NGram Model (triGram in particular) using Katz back-off smoothing technique.
 
-Then follow the instruction.
+#Usage
+
+Since i choose Nodejs for this project, you have to install it in order to make it runable:
+
+  apt-get install nodejs
+
+change your directory to my repository:
+
+  cd path_to_my_project
+
+Here we go:
+
+  node triGram.js
+
+Then follow the instruction with appropriate parameters.
+To train the model again: 
+  node triGram.js 1
+To re-calculate the perlexity:
+  node triGram.js 2
+
+The 3rd part (sentence generation) will be updated as soon as possible.
+//TODO: dynamic model selection also something could be improved
+
+I let it Open-source at: https://github.com/vinhnam/languageModeling
--- triGram.js
+++ triGram.js
@@ -7,6 +7,15 @@
 var lineReader = require('line-reader');
 var path = require('path');
 var cmlArgs = ["1", "2", "3"];
+
+var unigramG = {}; // global variables
+var bigramG = {};
+var trigramG = {};
+var bigramSeriesG = {};
+var trigramSeriesG = {};
+var globalWordCount = 0;
+var globalVarsLoaded = false;
+
 if (cmlArgs.indexOf(process.argv[2]) >= 0){
   switch(process.argv[2]){
     case '1':
@@ -15,10 +24,11 @@ if (cmlArgs.indexOf(process.argv[2]) >= 0){
       trigramModelCreator('reuters','reuters');
       break;
     case '2':
-      console.log("Calculating perplexity ... ");
+      console.log("Perplexity Calculation started ... ");
+      perplexityCalculation();
       break;
     case '3':
-      console.log("Generating a sentence... ");
+      console.log("Generating a sentence will be updated in my next version.");
       gerenateSentence();
       break;
   }
@@ -29,20 +39,134 @@ if (cmlArgs.indexOf(process.argv[2]) >= 0){
               "3. sentence generator");
 }
 
+function perplexityCalculation(){
+  if(!globalVarsLoaded){
+    console.log("Loading trained Model ...");
+    var fs = require('fs');
+    unigramG = JSON.parse(fs.readFileSync(__dirname +'/brown/brown_unigram', 'utf8'));
+    bigramG = JSON.parse(fs.readFileSync(__dirname +'/brown/brown_bigram', 'utf8'));
+    trigramG = JSON.parse(fs.readFileSync(__dirname +'/brown/brown_trigram', 'utf8'));
+    bigramSeriesG = JSON.parse(fs.readFileSync(__dirname +'/brown/brown_bigramSeries', 'utf8'));
+    trigramSeriesG = JSON.parse(fs.readFileSync(__dirname +'/brown/brown_trigramSeries', 'utf8'));
+    Object.keys(unigramG).forEach(function(key) {
+      globalWordCount += unigramG[key];
+    });   
+    console.log("Model Loaded!");                    
+  }  
+
+  var lineReader = require('line-reader');
+  var logProbability = 0;
+  var totalWord = 0; // M in slide
+  var sentenceTested = 0;
+  console.log("Start calculate probability of each sentence in test file ...");
+  lineReader.eachLine(__dirname +'/brown/brown'+'_test', function(line, last) {
+    sentenceTested++;
+    console.log("current process: "+ sentenceTested);
+    line += ' STOP';
+    probabilityObj = sentenceProbabilityCal(line); 
+    logProbability += Math.log(probabilityObj['p']);    
+    totalWord += probabilityObj['length'];
+    if (last){ 
+      l = logProbability / totalWord;
+      perplexity = Math.pow(2, (-1 * l));
+      console.log("************Report**************");
+      console.log("total Available word in vocabulary: " + unigramG.length);
+      console.log("total tested word: "+totalWord);
+      console.log("total sentence tested: "+ sentenceTested);
+      console.log("Perplexity at: " + perplexity);
+      return false; // stop reading
+    }
+  });
+}
+
+function availableWordChecker( sentence, index){
+  if (index < 0) return '*';
+  if(unigramG[sentence[index]] == undefined) return "<UNK>";
+  return sentence[index];
+}
+function sentenceProbabilityCal(sentence){
+    sentence = sentence.split(' ');
+    var sentenceProbality = 1;
+    for (var i = 1; i < sentence.length +1; i++ ) {
+      var word = availableWordChecker(sentence, (i-1) );
+      var lastWord = availableWordChecker(sentence, (i-2) );
+      var lastTwoWord = availableWordChecker(sentence, (i-3));
+      sentenceProbality *= trigramSequence(lastTwoWord, lastWord , word);
+    };
+    return {"p" : sentenceProbality, "length":sentence.length};
+}
+
+function trigramSequence(lastTwoWord, lastWord, word){ // q (wi | w i-1 , w i-2)
+  if(trigramSeriesG[lastTwoWord + ' ' + lastWord] == undefined) return 1; // not effect the others.
+  if(trigramSeriesG[lastTwoWord + ' ' + lastWord].length > 0){
+    if(trigramSeriesG[lastTwoWord + ' ' + lastWord].indexOf(word) > 0){
+      q = (trigramG[lastTwoWord+ ' ' + lastWord + ' ' + word] - 0.5) / bigramG[lastTwoWord + ' ' +  lastWord];
+    } else {
+      var alpha = bigramAlpha (lastTwoWord, lastWord);
+      var backOffWord = bigramSequence (lastWord, word);
+      var totalBackOffTrigram = 0;
+
+      trigramSeriesG[lastTwoWord +' '+ lastWord].forEach(function(key){
+        totalBackOffTrigram += bigramSequence(lastWord, word);
+      })
+      q = alpha * (backOffWord / (1 - totalBackOffTrigram));
+    }
+  } else {
+    q = bigramSequence(lastWord, word);
+  }
+  return q;
+}    
+
+function bigramSequence(lastWord, word){  // q (wi | w i-1 )
+  if(bigramSeriesG[lastWord].length > 0){
+    if(bigramSeriesG[lastWord].indexOf(word) > 0){
+      q = (bigramG[lastWord+' '+ word] - 0.5) / unigramG[lastWord];
+    }else{
+      var alpha = unigramAlpha(lastWord);
+      countStar = unigramG[word] || 1;
+      totalConnectedWordInBigram = 0; // set A
+      bigramSeriesG[lastWord].forEach(function(wi) {
+        totalConnectedWordInBigram += unigramG[wi];
+      });
+      totalWordNotConnectedToLastWordBigram = globalWordCount - totalConnectedWordInBigram; // set B
+      q = alpha * (countStar / totalWordNotConnectedToLastWordBigram );
+    } 
+  } else {
+    q = unigramG[word] / globalWordCount;
+  }
+  return q;
+}
+    
+function bigramAlpha(lastTwoWord, lastWord){
+  return alpha = (0.5 * trigramSeriesG[lastTwoWord + ' ' + lastWord].length ) / 
+        bigramG[lastTwoWord + ' ' + lastWord] || 0;
+}
+
+function unigramAlpha(lastWord){
+  return alpha = (0.5 * bigramSeriesG[lastWord].length ) / unigramG[lastWord] || 0;
+}
+
 function gerenateSentence(){
-  var fs = require('fs');
-  var trigram = fs.readFileSync(__dirname +'/brown/brown_trigram', 'utf8');
-  trigram = JSON.parse(trigram);
-  console.log(trigram);
+  if(!globalVarsLoaded){
+    var fs = require('fs');
+    unigramG = JSON.parse(fs.readFileSync(__dirname +'/brown/brown_unigram', 'utf8'));
+    bigramG = JSON.parse(fs.readFileSync(__dirname +'/brown/brown_bigram', 'utf8'));
+    trigramG = JSON.parse(fs.readFileSync(__dirname +'/brown/brown_trigram', 'utf8'));
+    bigramSeriesG = JSON.parse(fs.readFileSync(__dirname +'/brown/brown_bigramSeries', 'utf8'));
+    trigramSeriesG = JSON.parse(fs.readFileSync(__dirname +'/brown/brown_trigramSeries', 'utf8'));
+    globalVarsLoaded = true;
+    Object.keys(unigramG).forEach(function(key) {
+      globalWordCount += unigramG[key];
+    });     
+  }
 }
 
 function jsonToFile(trainingModel, trainingFile, jsonValue){
   fs = require('fs');
-  fs.writeFile(path.join(__dirname,'/'+trainingModel + '/' + trainingModel+ '_' + trainingFile ), jsonValue,{ flags: 'w+',
-              defaultEncoding: 'utf8'}, function (err) {
+  fs.writeFile(path.join(__dirname,'/'+trainingModel + '/' + trainingModel+ '_' + trainingFile ), 
+    jsonValue,{ flags: 'w+', defaultEncoding: 'utf8'}, function (err) {
     if (err) return console.log(err);
     console.log('Done writing, you can check it out at:');
-
     console.log(path.join(__dirname,'/'+trainingModel + '/' + trainingModel+ '_' + trainingFile ));
   });
 }
